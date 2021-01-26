f0418159379bb4a2c47aa76238d2def04a260b2a
Robert Hu <hhh1126@hotmail.com>
2021-01-26 17:28:09 +0800

    enable Fleet Contrack in QSR10G host driver (2)

--- patches/user/qtna_wifi_ax/module/qsr10g-driver-v7.0.0.7/common/pearl_vnet.c
+++ patches/user/qtna_wifi_ax/module/qsr10g-driver-v7.0.0.7/common/pearl_vnet.c
@@ -619,6 +619,10 @@ static inline void vmac_rx_forward(struct vmac_priv *vmp, uint32_t bdata,
 }
 #endif
 
+#if defined(CONFIG_FC_QTNA_WIFI_AX)
+extern int rtk_fc_fastfwd_netif_rx(struct sk_buff *skb);
+#endif
+
 static inline bool vmac_rxpkt_handup(struct net_device *ndev,
 		struct rx_buf_info_t *rxinfo, int len)
 {
@@ -659,7 +663,13 @@ static inline bool vmac_rxpkt_handup(struct net_device *ndev,
 
 	dump_rx_pkt(vmp, (char *)skb->data, (int)skb->len);
 	skb->protocol = eth_type_trans(skb, ndev);
+
+#if defined(CONFIG_FC_QTNA_WIFI_AX)
+        rtk_fc_fastfwd_netif_rx(skb);
+#else
 	netif_receive_skb(skb);
+#endif
+
 #endif
 	return true;
 }
